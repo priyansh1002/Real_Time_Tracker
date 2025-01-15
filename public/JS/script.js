aecfca9b08290afbf9ec3765326f2eaadf1199df
Priyansh <priyansh2001singh@gmail.com>
2025-01-15 22:29:12 +0530

    Tracking

--- public/JS/script.js
+++ public/JS/script.js
@@ -18,7 +18,7 @@ if (navigator.geolocation) {
 const map=L.map("map").setView([0,0],16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"OpenStreetMap"
+    attribution:"Tracking System"
 }).addTo(map)
 
 const markers={};
